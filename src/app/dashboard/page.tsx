f0fe7d1462f343f36ab0c99b0ac7b62135665c57
gumeeee <gaxco123@gmail.com>
2025-05-28 18:36:57 -0300

    feat: implement session verification on authentication page and redirection on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,22 @@
-const DashboardPage = () => {
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import SignOutButton from "./components/sign-out-button";
+import { redirect } from "next/navigation";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
       <h1>Dashboard Page</h1>
+      <SignOutButton />
     </div>
   );
 };
