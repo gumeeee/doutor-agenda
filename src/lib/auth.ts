a0371cc56c794d33647a160cc909b8b78ce9981c
gumeeee <gaxco123@gmail.com>
2025-05-29 00:24:29 -0300

    feat: add clinic data to user session and render it on sidebar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
 import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { customSession } from "better-auth/plugins";
 import * as schema from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -15,6 +17,29 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(schema.usersToClinicsTable.userId, user.id),
+        with: {
+          clinic: true,
+        },
+      });
+
+      // TODO: Mudar para retornar todas as clínicas do usuário. Caso aumntar o número de clínicas, mudar para retornar um array de clínicas.
+      const clinic = clinics[0];
+      return {
+        user: {
+          ...user,
+          clinic: {
+            id: clinic.clinicId,
+            name: clinic.clinic.name,
+          },
+        },
+        session,
+      };
+    }),
+  ],
   user: {
     modelName: "usersTable",
   },
